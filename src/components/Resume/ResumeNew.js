0140b3b1978a8d5e2b5bb2bc2798a384b285953b
100rbhsarin <saurabh007630@gmail.com>
2023-03-27 22:27:17 +0530

    "#"resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/100rbhsarin/Profile/main/src/Assets/Saurabh Kumar dev.pdf";
+  "https://raw.github.com/100rbhsarin/Profile/main/src/Assets/Saurabh Kumar dev.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
