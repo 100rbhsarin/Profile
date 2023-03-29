884568813e5ac7e26c654e0356bf519b3b98a610
100rbhsarin <saurabh007630@gmail.com>
2023-03-29 16:26:19 +0530

    "#" about img

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import Particle from "../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/s2.png";
+import laptopImg from "../../Assets/s8.png";
 import Toolstack from "./Toolstack";
 
 function About() {
